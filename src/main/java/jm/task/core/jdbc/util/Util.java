56e4d88ce3736ac38b12c57345461e361a896da5
Alexander <sasha.maks92@gmail.com>
2023-03-28 22:45:47 +0300

    +

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,21 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost/db_java_pp";
+    private static final String NAME = "user";
+    private static final String PASS = "user";
+    public static Connection getConnect(){
+        try {
+            return DriverManager.getConnection(URL,
+                    NAME,
+                    PASS);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
